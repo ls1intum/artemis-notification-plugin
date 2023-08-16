3126b49abff765c9a1316c14d3f9265860cbcf96
Benjamin Schmitz <benjamin.schmitz@tum.de>
2023-08-16 11:16:21 +0200

    Fix plugin not failing if post error

---
+++ src/main/java/de/tum/cit/ase/artemis_notification_plugin/exception/PostResultException.java
@@ -0,0 +1,8 @@
+package de.tum.cit.ase.artemis_notification_plugin.exception;
+
+public class PostResultException extends RuntimeException {
+
+    public PostResultException(Throwable cause) {
+        super(cause);
+    }
+}
