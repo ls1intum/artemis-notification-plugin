160834a9bff69fb27f8e57278aa8d19e5e70ff08
Benjamin Schmitz <benjamin.schmitz@tum.de>
2023-10-23 09:01:55 +0200

    Apply spotless

--- src/main/java/de/tum/cit/ase/artemis_notification_plugin/exception/TestParsingException.java
+++ src/main/java/de/tum/cit/ase/artemis_notification_plugin/exception/TestParsingException.java
@@ -1,6 +1,7 @@
 package de.tum.cit.ase.artemis_notification_plugin.exception;
 
 public class TestParsingException extends RuntimeException {
+
     public TestParsingException(Throwable cause) {
         super(cause);
     }
